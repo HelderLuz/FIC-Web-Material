515e91107f16dd5389504ad03f39cbec03c92447
HelderLuz <67494949+HelderLuz@users.noreply.github.com>
2025-11-05 20:00:29 -0300

    Update script.js

--- Aula 20 - Armazenamento/Ex4/script.js
+++ Aula 20 - Armazenamento/Ex4/script.js
@@ -1,6 +1,28 @@
 const form = document.querySelector('form')
+let animais = []
+
+if (localStorage.getItem('animais')) {
+    animais = JSON.parse(localStorage.getItem('animais'))
+
+    for (const animal of animais) {
+        adicionarAnimal(animal)
+    }
+}
+
 form.addEventListener('submit', (event) => {
     event.preventDefault()
 
-    console.log(form)
+    const formData = new FormData(form)
+    const animal = Object.fromEntries(formData.entries())
+
+    adicionarAnimal(animal)
+    animais.push(animal)
+    localStorage.setItem('animais', JSON.stringify(animais))
 })
+
+function adicionarAnimal({tipo, nome, idade, peso, cor}) {
+    const lista = document.querySelector('#lista')
+    const p = document.createElement('p')
+    p.textContent = `Tipo: ${tipo} | Nome: ${nome} | Idade: ${idade} | Peso: ${peso} | Cor: ${cor}`
+    lista.append(p)
+}
